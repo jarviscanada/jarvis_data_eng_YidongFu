82a1ded00447d9a88b4dd9b041ccdd6868b4aaab
Yidong Fu <davidfyd@gmail.com>
2020-08-06 15:53:07 +0000

    finished script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -8,14 +8,12 @@ usage=$1
 db_username=$2
 db_password=$3
 
+sudo centos
+
 systemctl status docker || systemctl start docker
 
-#hint: use case statement to handle argument
-#https://linuxize.com/post/bash-case-statement/
-#if $1 == "create"
+#if argument is create
 if [ "$1" = 'create'];then
-  #hint `docker container ls -a -f name=jrvs-psql | wc -l` should be 2
-  #if `jrvs-psql` container is already created
   if [ $(docker container ls -a -f name=jrvs-psql | wc -l) -eq 2 ]; then
     echo "container is already created"
     exit 1
@@ -24,28 +22,22 @@ if [ "$1" = 'create'];then
     echo "username or password is invalid"
     exit 1
 
-  #hint: `docker volume create ...`
   docker volume create pgdata
-  #hint: `docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_password} -e POSTGRES_USER=${db_username} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres`
   docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_password} -e POSTGRES_USER=${db_username} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-  #What's $? variable? https://bit.ly/2LanHUi
   exit $?
 
 if [ $(docker container ls -a -f name=jrvs-psql | wc -l) -eq 1 ]; then
     echo "container does not exist"
     exit 1
 
+#if argument is start
 if $1 = "start"
   if [ $(docker container ls -a -f name=jrvs-psql | wc -l) -eq 1 ]; then
      echo "container does not exist"
   docker container start jrvs-psql
   exti $?
 
+#if argument is stop
 if $1 = "stop"
   docker container stop jrvs-psql
   exit $?
-
-if [ $1 != "create" ] ||  [ $1 != "start" ] || [ $1 != "stop" ]
-  echo "It's an invalid argument"
-  exit 1
-
