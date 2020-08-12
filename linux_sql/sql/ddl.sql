b382801f94e47cae86eeae9763be7c916bd448eb
Yidong Fu <davidfyd@gmail.com>
2020-08-12 17:13:19 +0000

    Monitoring Agent

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -4,17 +4,17 @@ GO
 CREATE TABLE IF NOT EXIST host_agent.'host_info'(id SERIAL NOT NULL,
 hostname VARCHAR NOT NULL,
 cpu_number INT NOT NULL,
-cpu_architechture VARCHAR NOT NULL,
+cpu_architecture VARCHAR NOT NULL,
 cpu_model VARCHAR NOT NULL,
 cpu_mhz NUMERIC NOT NULL,
-L2_cache INT NOT NULL,
+L2_cache VARCHAR NOT NULL,
 total_mem INT NOT NULL,
 timestamp TIMESTAMP WITH TIME ZONE NOT NULL);
 
 CREATE TABLE IF NOT EXIST host_agent.'host_usage'("timestamp" TIMESTAMP NOT NULL,
 host_id SERIAL NOT NULL,
-memory_free INT NOT NULL,
+memory_free VARCHAR NOT NULL,
 cpu_idle INT NOT NULL,
 cpu_kernel INT NOT NULL,
 disk_io INT NOT NULL,
-disk_available INT NOT NULL);
+disk_available VARCHAR NOT NULL);
---
+++ scriptsample.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "About to try to access a file that doesn't exist"
+cat bad_filename.txt
